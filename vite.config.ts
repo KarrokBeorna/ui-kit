a3d1045963884d8139a18efd20df84a2597dd89d
Dmitriy Savel'ev <savelev_lotr@mail.ru>
2026-07-16 15:30:24 +0300

    d.ts generate added

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
+import dts from 'vite-plugin-dts'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts({
+      insertTypesEntry: true, // автоматически добавляет entry в package.json
+    }),
+  ],
   build: {
     lib: {
-      entry: path.resolve(__dirname, 'src/index.ts'),
+      entry: 'src/index.ts',
       name: 'UIKit',
       formats: ['es', 'cjs'],
       fileName: (format) => `index.${format === 'es' ? 'js' : 'cjs'}`,
     },
     rollupOptions: {
       external: ['react', 'react-dom'],
-      output: {
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
-        },
-      },
     },
-    cssCodeSplit: false, // собираем стили в один файл
-  },
-  css: {
-    modules: false,
   },
 })
